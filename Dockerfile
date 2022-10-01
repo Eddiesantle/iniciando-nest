6bf1bee0f4f33268f59a81e379e026392facaf9a
Eddielinux <wedleys8@gmail.com>
2022-10-01 12:42:23 -0300

    REFACTOR: Ajuste de port

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,12 @@
 FROM node:16.16.0-slim
 
-RUN npm install -g @nestjs/cli
+RUN npm install -g @nestjs/cli && npm update -g
 
 USER node
 
 WORKDIR /home/node/app
+
+COPY . .
 
 CMD [ "/home/node/app/start.sh" ]
 
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Constants
+const PORT = process.env.PORT || 3002;
+const HOST = '127.0.0.1';
+
+  await app.listen(PORT);
+  //await app.listen(PORT, HOST);
+  console.log(`Running on http://${HOST}:${PORT}`);
 }
 bootstrap();
